8ef9bc289ed8601b4e5d79c52533750cc759d252
tommyshi <tommytshi@gmail.com>
2020-06-29 15:59:37 -0700

    created React app for exercise 1.1 and part1 content

---
+++ part1/e1.1/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.course} </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.p} {props.e} </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+    <Part p={props.parts[0].name} e={props.parts[0].exercises}/>
+    <Part p={props.parts[1].name} e={props.parts[1].exercises}/>
+    <Part p={props.parts[2].name} e={props.parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+    name: 'Fundamentals of React',
+    exercises : 10
+    },
+    {
+    name: 'Using props to pass data',
+    exercises: 7
+    },
+    {
+    name: 'State of a component',
+    exercises: 14
+    }
+  ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
